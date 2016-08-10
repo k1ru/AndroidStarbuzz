bfca877977443104b0185eea1195b677ea962dc4
mdrb36 <mdrb36@motorolasolutions.com>
2016-08-10 16:12:23 +0300

    working copy 3

--- app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
@@ -1,7 +1,9 @@
 package com.hfad.starbuzz;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -10,11 +12,16 @@ public class DrinkCategoryActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drink_category);
 
         ArrayAdapter<Drink> listAdapter = new ArrayAdapter<Drink>(this, android.R.layout.simple_list_item_1, Drink.drinks);
-
         ListView listView = getListView();
         listView.setAdapter(listAdapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView listView, View itemView, int position, long id) {
+        Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+        intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int) id);
+        startActivity(intent);
     }
 }
